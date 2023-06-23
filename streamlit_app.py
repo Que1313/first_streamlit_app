cd0dcb8df3fbc0ad1aa7f221d70a3d7284ebc976
Que1313 <47263098+Que1313@users.noreply.github.com>
2023-06-23 11:51:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import Pandas
+
 
 streamlit.title('My Parents New Health Diner')
 
@@ -11,7 +11,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-
+import Pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 streamlit.dataframe(my_fruit_list)
